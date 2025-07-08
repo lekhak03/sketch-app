e74c9f879940096662c944dafd20dde6e202a9f1
Deep Lekhak <59480224+Deep03@users.noreply.github.com>
2025-07-08 11:02:03 -0400

    fix: module not found error vercel

--- src/hooks/useCanvas.ts
+++ src/hooks/useCanvas.ts
@@ -1,11 +1,5 @@
 import { useRef, useState, useCallback } from 'react';
 import { deduplicatePaths  } from './duplicateStrokes';
-import { writeData } from './setRealtimeDb'
-
-// for private self hosted server
-// url of the server
-// const URL = "ws://localhost:8080"
-// const socket = new WebSocket(URL)
 
 export type Tool = 'pen' | 'eraser';
 
@@ -98,12 +92,7 @@ export function useCanvas(backgroundColor: string) {
       const dataToBeSaved = deduplicatePaths(paths, existingDataParsed); // remove any duplicates
 
       localStorage.setItem('drawPaths', dataToBeSaved);
-
-      // for self hosted server, !firebase
-      // if (socket.readyState === WebSocket.OPEN) socket.send(JSON.stringify(paths.concat(existingDataParsed)));
     }
-    writeData(paths); // writes to firebase real time server
-    // writePersistentData(paths); // writes to firebase real time server
   }, [currentPath]);
 
   //   clear canvas
