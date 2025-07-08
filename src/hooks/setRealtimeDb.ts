5118a7f07bb1528ca2b3e4225e698e1a81379f2a
Deep Lekhak <59480224+Deep03@users.noreply.github.com>
2025-07-08 10:27:03 -0400

    feat: added db for persistent storage

--- src/hooks/setRealtimeDb.ts
+++ src/hooks/setRealtimeDb.ts
@@ -1,15 +1,27 @@
 import { getDatabase, ref, set } from 'firebase/database';
 import { Point } from './useCanvas'
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from './databaseConfig';
+import { firebaseConfig, firebasePersistentConfig } from './databaseConfig';
 
 // intiliaze the app, doesnt work without it
-initializeApp(firebaseConfig);
+const tempDb = initializeApp(firebaseConfig, "tempDb");
+const persistentDb = initializeApp(firebasePersistentConfig);
+
 
 // write paths to the DB
 export function writeData(paths: Point[][]) {
-  const db = getDatabase();
+  const db = getDatabase(tempDb);
   const reference = ref(db, 'paths/');
+
+  set(reference, {
+    drawpaths: paths
+  });
+}
+
+// write paths to the DB
+export function writePersistentData(paths: Point[][]) {
+  const db = getDatabase(persistentDb);
+  const reference = ref(db, 'persistentPaths/');
 
   set(reference, {
     drawpaths: paths
