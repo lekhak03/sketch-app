9f6e54a3d0db7fc7d94281c394adaad5ede63f31
Deep Lekhak <59480224+Deep03@users.noreply.github.com>
2025-07-09 23:25:32 -0400

    fix: add firebaseConfig for Vercel support

---
+++ src/hooks/databaseConfig.ts
@@ -0,0 +1,10 @@
+// config from Firebase SDK
+export const firebaseConfig = {
+  apiKey: "AIzaSyBaTd79Pfm2nGHR45c6OQsr8qLHTLngq9c",
+  authDomain: "sketch-pad-c3f3b.firebaseapp.com",
+  projectId: "sketch-pad-c3f3b",
+  storageBucket: "sketch-pad-c3f3b.firebasestorage.app",
+  messagingSenderId: "678322755049",
+  appId: "1:678322755049:web:85516e0c345e9a06f5e778",
+  measurementId: "G-41SBSWH1KS"
+};
