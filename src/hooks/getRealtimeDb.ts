ce8f497be56b3844779eb381c25cf0f83eb66d23
Deep Lekhak <59480224+Deep03@users.noreply.github.com>
2025-07-09 22:51:16 -0400

    chore: remove redudant code

--- src/hooks/getRealtimeDb.ts
+++ src/hooks/getRealtimeDb.ts
@@ -1,14 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getDatabase, ref, onValue} from "firebase/database";
 import { firebaseConfig } from './databaseConfig';
 
-
 initializeApp(firebaseConfig);
-
-const db = getDatabase();
-const starCountRef = ref(db, 'paths/');
-onValue(starCountRef, (snapshot) => {
-  const data = snapshot.val();
-  console.log(data)
-});
